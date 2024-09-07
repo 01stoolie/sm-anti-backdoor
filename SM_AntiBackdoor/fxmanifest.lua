705c5fa2e5ad36e8b0fef7e23c93d3a8cd1ac1e8
Stoolie <rajonstooliedsrp@gmail.com>
2024-09-06 21:29:27 -0700

    Add files via upload

---
+++ SM_AntiBackdoor/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author '_Stoolie'
+description 'detection of backdoors on a FiveM server using a Discord Webhook and other connections for notifications.'
+
+server_script 'server.lua'
